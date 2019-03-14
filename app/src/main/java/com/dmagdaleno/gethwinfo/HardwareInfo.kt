dad51896ab62a55d8b278f24cb38251309a79b15
dmagdaleno <diegommagdaleno@gmail.com>
2019-03-14 17:32:41 -0300

    try catch to prevent error when wifi is not connected

--- app/src/main/java/com/dmagdaleno/gethwinfo/HardwareInfo.kt
+++ app/src/main/java/com/dmagdaleno/gethwinfo/HardwareInfo.kt
@@ -1,13 +1,18 @@
 package com.dmagdaleno.gethwinfo
 
+import android.util.Log
 import com.dmagdaleno.gethwinfo.HardwareConst.Companion.INTERFACE_NAME
 import com.dmagdaleno.gethwinfo.extensions.mapToHexaString
+import java.lang.Exception
 import java.net.NetworkInterface
 
 fun getMACAddress(): String =
-    NetworkInterface.getNetworkInterfaces()
-        .asSequence()
-        .filter { it.name == INTERFACE_NAME }
-        .map { it.hardwareAddress }
-        .first()
-        .mapToHexaString()
+    try {
+        NetworkInterface.getNetworkInterfaces()
+            .asSequence()
+            .first { it.name == INTERFACE_NAME }.hardwareAddress
+            .mapToHexaString()
+    } catch (e: Exception) {
+        Log.e("MAC Address", "Not found", e)
+        "Not found"
+    }
