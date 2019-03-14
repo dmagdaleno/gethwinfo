dfed5d9ed6d71bcee961c8fd3c2be499c79b58c1
dmagdaleno <diegommagdaleno@gmail.com>
2019-03-14 16:52:08 -0300

    call function to get mac

--- app/src/main/java/com/dmagdaleno/gethwinfo/MainActivity.kt
+++ app/src/main/java/com/dmagdaleno/gethwinfo/MainActivity.kt
@@ -1,12 +1,8 @@
-package br.com.valid.bio.component.gethwinfo
+package com.dmagdaleno.gethwinfo
 
-import android.content.Context
-import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.net.NetworkInterface
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,35 +14,5 @@ class MainActivity : AppCompatActivity() {
 
         txt_mac.text = mac
     }
-
-    private fun getMACAddress(): String {
-        val interfaceName = "wlan0"
-
-        val interfaces = Collections.list(NetworkInterface.getNetworkInterfaces())
-
-        for (intf in interfaces) {
-
-            if (intf.name != interfaceName){
-                continue
-            }
-
-            val mac = intf.hardwareAddress ?: return ""
-
-            val buf = StringBuilder()
-
-            mac.forEach { m ->
-                buf.append(String.format("%02X:", m))
-            }
-
-            if (buf.isNotEmpty()) {
-                buf.deleteCharAt(buf.length - 1)
-            }
-
-            return buf.toString()
-        }
-
-        return ""
-    }
-
 
 }
